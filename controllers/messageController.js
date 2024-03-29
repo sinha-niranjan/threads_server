d8e17c251ad3a2685e9df1f766907c9f14e266ec
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-03-29 12:35:46 +0530

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   controllers/messageController.js
    	modified:   models/conversationModel.js
    	modified:   routes/messageRoutes.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -15,8 +15,6 @@ const sendMessage = async (req, res) => {
       return;
     }
 
-    console.log(senderId);
-
     let conversation = await Conversation.findOne({
       participants: { $all: [senderId, recipientId] },
     });
@@ -88,7 +86,6 @@ const getMessages = async (req, res) => {
 
 const getConversations = async (req, res) => {
   const userId = req.user._id;
-
   try {
     const conversations = await Conversation.find({
       participants: userId,
@@ -97,6 +94,12 @@ const getConversations = async (req, res) => {
       select: "username profilePic",
     });
 
+    // remove the current user from the participants array
+    conversations.forEach((conversation) => {
+      conversation.participants = conversation.participants.filter(
+        (participant) => participant._id.toString() !== userId.toString()
+      );
+    });
     res.status(200).json(conversations);
   } catch (error) {
     res.status(500).json({ error: error.message });
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getConversations, getMessages, sendMessage } from "../controllers/messageController.js";
+import {
+  getConversations,
+  getMessages,
+  sendMessage,
+} from "../controllers/messageController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
-
-router.get("/:otherUserId", protectRoute, getConversations);
-router.get("/conversations", protectRoute, getMessages);
+router.get("/conversations", protectRoute, getConversations);
+router.get("/:otherUserId", protectRoute, getMessages);
 router.post("/", protectRoute, sendMessage);
 
 export default router;
