b061f1f5de3d782cb3c74b71d3394208e250e798
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-03-30 12:51:27 +0530

    Changes to be committed: 	modified:   controllers/userController.js 	modified:   routes/userRoutes.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -251,6 +251,43 @@ const updateUser = async (req, res) => {
   }
 };
 
+// getSuggestedUsers --------------------------------------------------------
+
+const getSuggestedUsers = async (req, res) => {
+  try {
+    // exclude the current uesr from suggested users array , exclude users that current user is already following
+
+    const userId = req.user._id;
+
+    const usersFollowedByYou = await User.findById(userId).select("followings");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      {
+        $sample: { size: 10 },
+      },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByYou.followings.includes(user._id)
+    );
+
+    const suggestedUsers = filteredUsers.slice(0, 5);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
 export {
   signupUser,
   loginUser,
@@ -258,4 +295,5 @@ export {
   followUnFollowUser,
   updateUser,
   getUserProfile,
+  getSuggestedUsers,
 };
